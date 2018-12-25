f45a1bd39f61d61b16a4bb7089e9cb67d191b57f
liucongxi@cloudhua.com <liucongxi@cloudhua.com>
2018-12-25 18:49:11 +0800

    弓箭手

--- src/game/MapBG.ts
+++ src/game/MapBG.ts
@@ -2,15 +2,10 @@
  * 游戏背景地图
  */
 class MapBG extends eui.Component {
-	/**地图块数组*/
-	private mapArray:Array<any>;
-	public mapBG:eui.Image;
+	public mapbg:eui.Image;
 	protected createChildren() {
 		this.skinName = "GameBGSkin";
 	}
-	public constructor() {
-		super();
-	}
+	//
 
-	
 }
