7334a30f13649cc52343d7d62aea82176ee4eca2
liucongxi@cloudhua.com <liucongxi@cloudhua.com>
2018-12-25 18:47:46 +0800

    地图资源

--- src/game/MapBG.ts
+++ src/game/MapBG.ts
@@ -2,9 +2,15 @@
  * 游戏背景地图
  */
 class MapBG extends eui.Component {
-	public mapbg:eui.Image;
+	/**地图块数组*/
+	private mapArray:Array<any>;
+	public mapBG:eui.Image;
 	protected createChildren() {
 		this.skinName = "GameBGSkin";
 	}
+	public constructor() {
+		super();
+	}
+
 	
 }
