1401aaf6ae445e4e8b7a19d868330293eedc62e7
wangfeng <wangfeng@cloudhua.com>
2019-01-09 19:39:13 +0800

    添加英雄列表组件

---
+++ src/game/HeroItem.ts
@@ -0,0 +1,18 @@
+/**
+ * 商城单个英雄
+*/
+
+class HeroItem extends eui.ItemRenderer {
+    public constructor() {
+        super();
+    }
+    protected dataChanged(): void {
+        super.dataChanged();
+    }
+    protected createChildren() {
+        super.createChildren();
+
+
+    }
+}
+window["HeroItem"] = HeroItem;
