5babe059a36fbefdc6d41c0a3ca789f3b255ae10
wangfeng <wangfeng@cloudhua.com>
2019-01-10 15:58:29 +0800

    添加英雄列表数据

--- src/game/HeroView.ts
+++ src/game/HeroView.ts
@@ -17,36 +17,35 @@ class HeroView extends eui.Component {
         super.createChildren();
         this.skinName = "HeroViewSkin";
         this.closeBtn.addEventListener(egret.TouchEvent.TOUCH_TAP, Main.instance.releaseHeroView, Main.instance);
-
-        // let ac = this.list.dataProvider as eui.ArrayCollection;
-        // ac.removeAll();
-
-
-
+        let ac = this.list.dataProvider as eui.ArrayCollection;
+        ac.removeAll();
+        let heroConfig = GameConfig.playerConfig.hero;
+        let config: Array<any> = heroConfig.config;
         let index = 0;
-        // let items: ITruegasItemInfoData[] = [];
-
-        // if (truegasList.length > 15) {
-
-        // 	this.scroller.scrollPolicyV = "on";
-        // } else {
-        // 	this.scroller.scrollPolicyV = "off";
-        // }
-
-        // for (let i = 0; i < 11; i++) {
-        // 	if (index < 4) {
-        // 		items.push(truegasList[i])
-        // 		index++;
-        // 	} else {
-        // 		ac.addItem({ items: items })
-        // 		items = [];
-        // 		items.push(truegasList[i])
-        // 		index = 1;
-        // 	}
-        // }
-        // if (items.length > 0) {
-        // 	ac.addItem({ items: items })
-        // }
+     
+        let items: any[] = [];
+        if (config.length > 2) {
+        	this.scroller.scrollPolicyV = "on";
+        } else {
+        	this.scroller.scrollPolicyV = "off";
+        }
+        for (let i = 0; i < config.length; i++) {
+            let obj = {
+                index: i, name: config[i].work, hp: config[i].hp, isSelect: (i == 0), gold: config[i].gold
+            }
+            if (index < 4) {
+                items.push(obj)
+                index++;
+            } else {
+                ac.addItem({ items: items })
+                items = [];
+                items.push(obj)
+                index = 1;
+            }
+        }
+        if (items.length > 0) {
+            ac.addItem({ items: items })
+        }
 
     }
 }
